6b45d2fa7d756d94ab60beff56b78f820a398fd9
Heisel16 <125604414+Heisel16@users.noreply.github.com>
2026-06-13 23:05:15 -0500

    actualizaciones otras

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ...(process.env.BUILD_TARGET === 'capacitor' ? { output: 'export' } : {}),
-  trailingSlash: true,
   images: {
     unoptimized: true
   }
